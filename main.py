6e9aac9f92d8693ef46222aa7f9f80df64589201
wardtd2 <wardtd@gmail.com>
2024-02-29 20:11:52 -0500

    Added function to split the characters to repot the counts in a report, then wrote the report

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ def main():
     text = get_book_text(path)
     print(f"Word count: {word_count(text)}")
     print(f"Counts of each character: {letter_count(text)}")
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count(text)} words found in the document")
+    letters = split_letters(letter_count(text))
+    for letter in letters:
+         print(f"The '{letter["character"]}' character was found {letter["count"]} times")
+    print("--- End report ---")
     
 
 def get_book_text(path):
@@ -24,6 +30,20 @@ def letter_count(text):
         else:
             letters[letter] = 1
     return letters
+
+
+def split_letters(dict):
+    letters = []
+    for character in dict:
+        if character.isalpha():
+            letters.append({"character": character, "count" : dict[character]})
+    letters.sort(reverse=True, key=sort_on)
+    return letters
+
+
+
+def sort_on(dict):
+    return dict["count"]
 
 
 main()
